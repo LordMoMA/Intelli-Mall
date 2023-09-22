264ccae2fc4c239a13130829fac054583baa0e5e
LordMoMA <lihongyi0512@gmail.com>
2023-09-22 11:36:01 +0800

    “added-comments”

--- baskets/internal/domain/basket_repository.go
+++ baskets/internal/domain/basket_repository.go
@@ -4,6 +4,16 @@ import (
 	"context"
 )
 
+/*
+the Load method is not defined in the BasketRepository interface itself.
+The Load method is only defined in the concrete implementations of the BasketRepository interface,
+such as FakeBasketRepository.
+
+The purpose of the BasketRepository interface is to define a set of methods
+that must be implemented by any concrete type that implements the interface.
+In this case, the BasketRepository interface defines the Load and Save methods,
+which must be implemented by any concrete type that implements the BasketRepository interface.
+*/
 type BasketRepository interface {
 	Load(ctx context.Context, basketID string) (*Basket, error)
 	Save(ctx context.Context, basket *Basket) error
